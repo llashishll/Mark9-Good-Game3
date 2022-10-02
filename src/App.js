797e3813975017ce721043f16742739ad98c3d38
Ashish Kalra <ashishkalra999@gmail.com>
2022-10-02 10:22:19 +0000

    Code Refactor

--- src/App.js
+++ src/App.js
@@ -5,71 +5,71 @@ const gameLibrary = {
   OpenWorld: [
     {
       name: "Grand Theft Auto 5",
-      metascore: "96"
+      metascore: "96",
     },
     {
       name: "Mafia 3",
-      metascore: "68"
+      metascore: "68",
     },
     {
       name: "Assassin's Creed Valhalla",
-      metascore: "84"
+      metascore: "84",
     },
     {
       name: "Days Gone",
-      metascore: "76"
+      metascore: "76",
     },
     {
       name: "Ghost of Tsuhima",
-      metascore: "83"
-    }
+      metascore: "83",
+    },
   ],
 
   FPS: [
     {
       name: "Call of Duty : Vanguard",
-      metascore: "73"
+      metascore: "73",
     },
 
     {
       name: "Battlefield 2042",
-      metascore: "68"
+      metascore: "68",
     },
     {
       name: "Tom Clancy's Rainbow Six Siege",
-      metascore: "73"
+      metascore: "73",
     },
     {
       name: "Apex Legends",
-      metascore: "88"
+      metascore: "88",
     },
     {
       name: "Halo Infinite",
-      metascore: "87"
-    }
+      metascore: "87",
+    },
   ],
   RPG: [
     {
       name: "The Witcher:Wild Hunt",
-      metascore: "93"
+      metascore: "93",
     },
     {
       name: "Elden Ring",
-      metascore: "94"
+      metascore: "94",
     },
     {
       name: "Fallout 4",
-      metascore: " 84"
+      metascore: " 84",
     },
     {
       name: "CyberPunk",
-      metascore: "86"
+      metascore: "86",
     },
     {
       name: "The Elder Scroll V:Skyrim",
-      metascore: "94"
-    }
-  ]
+      metascore: "94",
+    },
+  ],
 };
 
 export default function App() {
@@ -95,7 +95,7 @@ export default function App() {
               padding: "0.5rem  1rem",
               border: "1px solid black",
               margin: "1rem 0.3rem",
-              textAlign: "center"
+              textAlign: "center",
             }}
           >
             {genre}
@@ -105,9 +105,9 @@ export default function App() {
       <hr />
       <div>
         <ul>
-          {gameLibrary[selectedGenre].map((book) => (
+          {gameLibrary[selectedGenre].map((game) => (
             <li
-              key={book.name}
+              key={game.name}
               style={{
                 display: "flex",
                 flexDirection: "column",
@@ -117,16 +117,14 @@ export default function App() {
                 padding: "1rem",
                 border: "1px solid #D1D5DB",
                 width: "70%",
-                borderRadius: "0.5rem"
+                borderRadius: "0.5rem",
               }}
             >
-              {" "}
-              <div style={{ fontSize: "larger" }}> {book.name} </div>
+              <div style={{ fontSize: "larger" }}> {game.name} </div>
               <div style={{ fontSize: "smaller" }}>
-                {" "}
-                Metascore:{book.metascore}{" "}
+                Metascore:{game.metascore}{" "}
               </div>
-              <div>{book.image}</div>
+              <div>{game.image}</div>
             </li>
           ))}
         </ul>
